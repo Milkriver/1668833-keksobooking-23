2a17b5fd33d2d4ad180fd6456b57897a26871a8d
Marina Churkina <mapav4@yandex.ru>
2021-06-07 13:21:50 +0500

    Правки по ошибкам

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const getRandomInteger = function (min, max) {
-  if (min < 0 || min > max)
+  if (min < 0 || min > max) {
     return undefined;
-
+  }
   const random = min + Math.random() * (max - min);
   return Math.round(random);
 };
 
 const getRandomDecimal = function (min, max, quantity) {
-  if (min < 0 || min > max)
+  if (min < 0 || min > max) {
     return undefined;
-
+  }
   const random = min + Math.random() * (max - min);
   return Number(random.toFixed(quantity));
 };
