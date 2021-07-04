4e3a7231019a201d7242b97bffec0d5ae21d761f
Marina Churkina <mapav4@yandex.ru>
2021-07-04 23:59:10 +0500

    Offer на карте

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 import { getRandomCards } from './data.js';
 import { renderSingleOffer } from './utils/generate.js';
 getRandomCards();
-renderSingleOffer();
+
+const offer =  {
+  title: 'Квартира студия в престижном районе',
+  address: 'Chiyoda-ku, Tōkyō-to 102-0091',
+  price: 88000,
+  type: 'house',
+  rooms: 6,
+  guests: 5,
+  checkin: '7:00',
+  checkout: '10:00',
+  features: [
+    'wifi',
+    'washer',
+    'elevator',
+    'parking',
+    'dishwasher',
+    'conditioner',
+  ],
+  description: 'Комната в трёхкомнатной квартире, подойдёт молодым путешественникам.',
+  photos: [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/cameron-venti-R64qgQ6rr_o.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  ],
+};
+
+renderSingleOffer(offer);
--- js/utils/generate.js
+++ js/utils/generate.js
@@ -1,50 +1,82 @@
-let makeElement = function (tagName, className, text) {
-  let element = document.createElement(tagName);
+const makeElement = function (tagName, className, text) {
+  const element = document.createElement(tagName);
   element.classList.add(className);
   if (text) {
     element.textContent = text;
-  };
+  }
   return element;
-}
+};
 
-let renderSingleOffer = function () {
-  let mapOffer = document.querySelector('.map__canvas');
-  let article = makeElement('article', 'popup');
+const renderSingleOffer = function (offer) {
+  const mapOffer = document.querySelector('.map__canvas');
+  const article = makeElement('article', 'popup');
   mapOffer.appendChild(article);
-  let title = makeElement('h3', 'popup__title', 'Уютное гнездышко для молодоженов');
+  const title = makeElement('h3', 'popup__title', offer.title);
   article.appendChild(title);
-  let address = makeElement('p', 'popup__text--address', '102-0082 Tōkyō-to, Chiyoda-ku, Ichibanchō, 14−3');
+  const address = makeElement('p', 'popup__text--address', offer.address);
   article.appendChild(address);
-  let price = makeElement('p', 'popup__text--price', '5200₽/ночь');
+  const price = makeElement('p', 'popup__text--price', `${offer.price  } ₽/ночь`);
   article.appendChild(price);
-  let type = makeElement('h4', 'popup__type', 'Квартира');
+  let type = makeElement('h4', 'popup__type', offer.type);
+  if (offer.type === 'flat') {
+    type = makeElement('h4', 'popup__type', 'Квартира');
+  }
+  if (offer.type === 'bungalow') {
+    type = makeElement('h4', 'popup__type', 'Бунгало ');
+  }
+  if (offer.type === 'house') {
+    type = makeElement('h4', 'popup__type', 'Дом');
+  }
+  if (offer.type === 'palace') {
+    type = makeElement('h4', 'popup__type', 'Дворец ');
+  }
+  if (offer.type === 'hotel') {
+    type = makeElement('h4', 'popup__type', 'Отель');
+  }
   article.appendChild(type);
-  let textСapacity = makeElement('p', 'popup__text--capacity', '2 комнаты для 3 гостей');
+  const textСapacity = makeElement('p', 'popup__text--capacity', `${offer.rooms  } комнат для ${  offer.guests  } гостей`);
   article.appendChild(textСapacity);
-  let textTime = makeElement('p', 'popup__text--time', 'Заезд после 14:00, выезд до 10:00');
+  const textTime = makeElement('p', 'popup__text--time', `Заезд после ${  offer.checkin  }, выезд до ${  offer.checkout}`);
   article.appendChild(textTime);
-  let features = makeElement('ul', 'popup__features');
+
+  const features = makeElement('ul', 'popup__features');
   article.appendChild(features);
-  let featuresWifi = makeElement('li', 'popup__feature--wifi', 'wi-fi');
-  features.appendChild(featuresWifi);
-  let featuresDishwasher = makeElement('li', 'popup__feature--dishwasher', 'dishwasher');
-  features.appendChild(featuresDishwasher);
-  let featuresParking = makeElement('li', 'popup__feature--parking', 'parking');
-  features.appendChild(featuresParking);
-  let featuresWasher = makeElement('li', 'popup__feature--washer', 'washer');
-  features.appendChild(featuresWasher);
-  let featuresElevator = makeElement('li', 'popup__feature--elevator', 'elevator');
-  features.appendChild(featuresElevator);
-  let featuresConditioner = makeElement('li', 'popup__feature--conditioner', 'conditioner');
-  features.appendChild(featuresConditioner);
-  let description = makeElement('p', 'popup__description', 'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.');
+  for (let index = 0; index < offer.features.length; index++) {
+    const feature = offer.features[index];
+    if (feature === 'wifi') {
+      const featuresWifi = makeElement('li', 'popup__feature--wifi', 'WiFi');
+      features.appendChild(featuresWifi);
+    }
+    if (feature === 'dishwasher') {
+      const featuresDishwasher = makeElement('li', 'popup__feature--dishwasher', 'посудомоечная машина');
+      features.appendChild(featuresDishwasher);
+    }
+    if (feature === 'washer') {
+      const featuresWasher = makeElement('li', 'popup__feature--washer', 'стиральная машина');
+      features.appendChild(featuresWasher);
+    }
+    if (feature === 'elevator') {
+      const featuresElevator = makeElement('li', 'popup__feature--elevator', 'лифт');
+      features.appendChild(featuresElevator);
+    }
+    if (feature === 'parking') {
+      const featuresParking = makeElement('li', 'popup__feature--parking', 'парковка');
+      features.appendChild(featuresParking);
+    }
+    if (feature === 'conditioner') {
+      const featuresConditioner = makeElement('li', 'popup__feature--conditioner', 'кондиционер');
+      features.appendChild(featuresConditioner);
+    }
+  }
+
+  const description = makeElement('p', 'popup__description', 'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.');
   article.appendChild(description);
-  let photos = makeElement('div', 'popup__photos');
+  const photos = makeElement('div', 'popup__photos');
   article.appendChild(photos);
-  let photosImg = makeElement('img', 'popup__photo');
+  const photosImg = makeElement('img', 'popup__photo');
   photosImg.src = document.querySelector('.popup__photo').imgUrl;
   photosImg.alt = document.querySelector('.popup__photo').text;
   article.appendChild(photosImg);
-}
+};
 
 export { renderSingleOffer };
