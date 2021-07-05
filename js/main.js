ce4b66fd34bd3e38cc57b8116031a2308e5d6fcf
Marina Churkina <mapav4@yandex.ru>
2021-07-05 13:34:09 +0500

    Функция renderSingleCard

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
 import { getRandomCards } from './data.js';
-import { renderSingleOffer } from './utils/generate.js';
+import { renderSingleCard as renderSingleCard } from './utils/generate.js';
 getRandomCards();
-
-const offer = {
-  title: 'Квартира студия в престижном районе',
-  address: 'Chiyoda-ku, Tōkyō-to 102-0091',
-  price: 88000,
-  type: 'house',
-  rooms: 6,
-  guests: 5,
-  checkin: '7:00',
-  checkout: '10:00',
-  features: [
-    'wifi',
-    'washer',
-    'elevator',
-    'parking',
-    'dishwasher',
-    'conditioner',
-  ],
-  description: 'Комната в трёхкомнатной квартире, подойдёт молодым путешественникам.',
-  photos: [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/cameron-venti-R64qgQ6rr_o.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ],
-};
-
-const author = {
-  avatar: "img/avatars/user04.png"
-};
-
-renderSingleOffer(offer);
+const card = getRandomCards(0);
+renderSingleCard(card);
--- js/utils/generate.js
+++ js/utils/generate.js
@@ -7,10 +7,12 @@ const makeElement = function (tagName, className, text) {
   return element;
 };
 
-const renderSingleOffer = function (offer) {
+const renderSingleCard = function (card) {
+  let offer = card.offer;
   const mapOffer = document.querySelector('.map__canvas');
-  const article = makeElement('article', 'popup');
+  const article = makeElement('img', 'popup__avatar');
   mapOffer.appendChild(article);
+  const avatar = makeElement('article', 'popup');
   const title = makeElement('h3', 'popup__title', offer.title);
   article.appendChild(title);
   const address = makeElement('p', 'popup__text--address', offer.address);
@@ -18,22 +20,25 @@ const renderSingleOffer = function (offer) {
   const price = makeElement('p', 'popup__text--price', `${offer.price} ₽/ночь`);
   article.appendChild(price);
   let type = makeElement('h4', 'popup__type', offer.type);
-  if (offer.type === 'flat') {
-    type = makeElement('h4', 'popup__type', 'Квартира');
-  }
-  if (offer.type === 'bungalow') {
-    type = makeElement('h4', 'popup__type', 'Бунгало ');
-  }
-  if (offer.type === 'house') {
-    type = makeElement('h4', 'popup__type', 'Дом');
-  }
-  if (offer.type === 'palace') {
-    type = makeElement('h4', 'popup__type', 'Дворец ');
-  }
-  if (offer.type === 'hotel') {
-    type = makeElement('h4', 'popup__type', 'Отель');
-  }
+  switch (offer.type) {
+    case 'flat':
+      type = makeElement('h4', 'popup__type', 'Квартира');
+      break;
+    case 'bungalow':
+      type = makeElement('h4', 'popup__type', 'Бунгало');
+      break;
+    case 'house':
+      type = makeElement('h4', 'popup__type', 'Дом');
+      break;
+    case 'palace':
+      type = makeElement('h4', 'popup__type', 'Дворец');
+      break;
+    case 'hotel':
+      type = makeElement('h4', 'popup__type', 'Отель');
+      break;
+  };
   article.appendChild(type);
+
   const textСapacity = makeElement('p', 'popup__text--capacity', `${offer.rooms} комнат для ${offer.guests} гостей`);
   article.appendChild(textСapacity);
   const textTime = makeElement('p', 'popup__text--time', `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
@@ -43,29 +48,31 @@ const renderSingleOffer = function (offer) {
   article.appendChild(features);
   for (let index = 0; index < offer.features.length; index++) {
     const feature = offer.features[index];
-    if (feature === 'wifi') {
-      const featuresWifi = makeElement('li', 'popup__feature--wifi', 'WiFi');
-      features.appendChild(featuresWifi);
-    }
-    if (feature === 'dishwasher') {
-      const featuresDishwasher = makeElement('li', 'popup__feature--dishwasher', 'посудомоечная машина');
-      features.appendChild(featuresDishwasher);
-    }
-    if (feature === 'washer') {
-      const featuresWasher = makeElement('li', 'popup__feature--washer', 'стиральная машина');
-      features.appendChild(featuresWasher);
-    }
-    if (feature === 'elevator') {
-      const featuresElevator = makeElement('li', 'popup__feature--elevator', 'лифт');
-      features.appendChild(featuresElevator);
-    }
-    if (feature === 'parking') {
-      const featuresParking = makeElement('li', 'popup__feature--parking', 'парковка');
-      features.appendChild(featuresParking);
-    }
-    if (feature === 'conditioner') {
-      const featuresConditioner = makeElement('li', 'popup__feature--conditioner', 'кондиционер');
-      features.appendChild(featuresConditioner);
+    switch (feature) {
+      case 'wifi':
+        const featuresWifi = makeElement('li', 'popup__feature--wifi', 'WiFi');
+        features.appendChild(featuresWifi);
+        break
+      case 'dishwasher':
+        const featuresDishwasher = makeElement('li', 'popup__feature--dishwasher', 'посудомоечная машина');
+        features.appendChild(featuresDishwasher);
+        break
+      case 'washer':
+        const featuresWasher = makeElement('li', 'popup__feature--washer', 'стиральная машина');
+        features.appendChild(featuresWasher);
+        break
+      case 'elevator':
+        const featuresElevator = makeElement('li', 'popup__feature--elevator', 'лифт');
+        features.appendChild(featuresElevator);
+        break
+      case 'parking':
+        const featuresParking = makeElement('li', 'popup__feature--parking', 'парковка');
+        features.appendChild(featuresParking);
+        break
+      case 'conditioner':
+        const featuresConditioner = makeElement('li', 'popup__feature--conditioner', 'кондиционер');
+        features.appendChild(featuresConditioner);
+        break
     }
   }
   const description = makeElement('p', 'popup__description', offer.description);
@@ -75,5 +82,5 @@ const renderSingleOffer = function (offer) {
     const photo = offer.photos[index];
   }
   article.appendChild(photos);
-};
-export { renderSingleOffer };
+}
+export { renderSingleCard };
