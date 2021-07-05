c28228409c2df4229fd2575d102cd08bb4011c69
Marina Churkina <mapav4@yandex.ru>
2021-07-05 18:22:13 +0500

    generate.js разделен на 2 файла

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { getRandomCards } from './data.js';
-import { renderSingleCard as renderSingleCard } from './utils/generate.js';
+import { renderSingleCard as renderSingleCard } from './services/render.js';
 const cards = getRandomCards();
 const card = cards[0];
 renderSingleCard(card);
