6f1aadab57b19b112c786c4bf34b71e7c2201f48
Marina Churkina <mapav4@yandex.ru>
2021-07-08 15:33:04 +0500

    Добавляет возможность активировать форму

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import { getRandomCards } from './data.js';
 import { renderCard } from './services/render.js';
-import { disabledForm } from './services/form-state.js'
+import { setFormState } from './services/form-state.js'
 const cards = getRandomCards();
 const card = cards[0];
 renderCard(card);
-disabledForm();
+setFormState(true);
+setTimeout(setFormState, 3000, false);
