b3492f47aa3799dbc7b3be9216d4d7d3548b30ee
Marina Churkina <mapav4@yandex.ru>
2021-07-10 20:01:34 +0500

    Рефакторинг кода в form.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import { getRandomCards } from './data.js';
 import { renderCard } from './services/render.js';
-import { setFormState } from './services/form-state.js';
+import { activateForm, deactivateForm } from './services/form.js';
 const cards = getRandomCards();
 const card = cards[0];
 renderCard(card);
-setFormState(true);
-setTimeout(setFormState, 3000, false);
+deactivateForm(true);
+setTimeout(activateForm, 3000, false);
