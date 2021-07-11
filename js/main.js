5eadf23e2fc5339eeb14e39f3088005ed784151e
Marina Churkina <mapav4@yandex.ru>
2021-07-11 13:28:41 +0500

    Добавлена валидация формы

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import { getRandomCards } from './data.js';
 import { renderCard } from './services/render.js';
-import { activateForm, deactivateForm } from './services/form.js';
+import { activateForm, deactivateForm, validateGuests } from './services/form.js';
 const cards = getRandomCards();
 const card = cards[0];
 renderCard(card);
 deactivateForm(true);
 setTimeout(activateForm, 3000, false);
+validateGuests();
--- js/services/form.js
+++ js/services/form.js
@@ -27,4 +27,60 @@ const deactivateForm = () => {
   filter.classList.add('map__filters--disabled');
 }
 
-export { activateForm, deactivateForm };
+const roomNumberSelect = form.querySelector("#room_number");
+const capacitySelect = form.querySelector("#capacity");
+
+const timein = form.querySelector("#timein");
+const timeout = form.querySelector("#timeout");
+
+timein.addEventListener('change', () => {
+  timeout.value = timein.value;
+});
+
+timeout.addEventListener('change', () => {
+  timein.value = timeout.value;
+});
+
+const capacityNotForGuests = capacitySelect.querySelector('option[value="0"]');
+const capacity1Guests = capacitySelect.querySelector('option[value="1"]');
+const capacity2Guests = capacitySelect.querySelector('option[value="2"]');
+const capacity3Guests = capacitySelect.querySelector('option[value="3"]');
+
+const disableCapacityOptions = () => {
+  capacityNotForGuests.disabled = true;
+  capacity1Guests.disabled = true;
+  capacity2Guests.disabled = true;
+  capacity3Guests.disabled = true;
+}
+
+const validation = {
+  1: () => {
+    capacity1Guests.disabled = false;
+    capacity1Guests.selected = true;
+  },
+  2: () => {
+    capacity1Guests.disabled = false;
+    capacity2Guests.disabled = false;
+    capacity1Guests.selected = true;
+  },
+  3: () => {
+    capacity1Guests.disabled = false;
+    capacity2Guests.disabled = false;
+    capacity3Guests.disabled = false;
+    capacity1Guests.selected = true;
+  },
+  100: () => {
+    capacityNotForGuests.disabled = false;
+    capacityNotForGuests.selected = true;
+  },
+}
+
+const validateGuests = () => {
+  const selectedOption = roomNumberSelect.options[roomNumberSelect.selectedIndex];
+  disableCapacityOptions();
+  validation[selectedOption.value]();
+}
+
+roomNumberSelect.addEventListener('change', validateGuests);
+
+export { activateForm, deactivateForm, validateGuests };
