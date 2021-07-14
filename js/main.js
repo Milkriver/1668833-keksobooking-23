27d5d975336154ba09f96736b57e3945d469809f
Marina Churkina <mapav4@yandex.ru>
2021-07-14 22:09:38 +0500

    Добавлена карта

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ renderCard(card);
 deactivateForm(true);
 setTimeout(activateForm, 1000, false);
 validateGuests();
+
+const map = L.map('map-canvas')
+.on('load', () => {
+  console.log('Карта инициализирована');
+})
+  .setView({
+    lat: 35.6895,
+    lng: 139.69171,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const marker = L.marker(
+  {
+    lat: 35.6895,
+    lng: 139.69171,
+  },
+  {
+    draggable: true,
+  },
+);
+
+marker
+.addTo(map);
+
+marker.on('moveend', (evt) => {
+  console.log(evt.target.getLatLng());
+});
+
