cb24a3beb7fee25a4cd255f5d0f42c5d4df67d8f
Marina Churkina <mapav4@yandex.ru>
2021-07-15 19:43:26 +0500

    Добавлены метки

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ import { renderCard } from './services/render.js';
 import { activateForm, deactivateForm, validateGuests } from './services/form.js';
 const cards = getRandomCards();
 const card = cards[0];
-renderCard(card);
+const offer = renderCard(card);
 deactivateForm(true);
-setTimeout(activateForm, 1000, false);
+// setTimeout(activateForm, 1000, false);
 validateGuests();
 
 const map = L.map('map-canvas')
 .on('load', () => {
+  activateForm(true);
   console.log('Карта инициализирована');
 })
   .setView({
@@ -24,20 +25,57 @@ L.tileLayer(
   },
 ).addTo(map);
 
-const marker = L.marker(
-  {
-    lat: 35.6895,
-    lng: 139.69171,
-  },
+const setAddressValue = (coordinateObject) => {
+  document.querySelector('#address').value =
+    `Широта: ${coordinateObject.lat.toFixed(3)}, долгота: ${coordinateObject.lng.toFixed(3)}`;
+}
+
+const defaultCoordinate = {
+  lat: 35.6895,
+  lng: 139.69171,
+}
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+const mainPinMarker = L.marker(
+  defaultCoordinate,
   {
     draggable: true,
+    icon: mainPinIcon,
   },
 );
 
-marker
-.addTo(map);
+setAddressValue(defaultCoordinate);
 
-marker.on('moveend', (evt) => {
-  console.log(evt.target.getLatLng());
+mainPinMarker
+.addTo(map)
+.bindPopup('Главная точка');
+
+mainPinMarker.on('moveend', (evt) => {
+  let coordinates = evt.target.getLatLng();
+  setAddressValue(coordinates);
 });
 
+const icon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+const marker = L.marker(
+  {
+    lat: 35.720,
+    lng: 139.692,
+  },
+  {
+    icon: icon,
+  },
+);
+
+setAddressValue(defaultCoordinate);
+
+marker
+.addTo(map)
+.bindPopup('offer.value');
