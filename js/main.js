f4bfe211aebb982ea50af288521525630078feb7
Marina Churkina <mapav4@yandex.ru>
2021-07-22 13:16:22 +0500

    Правки

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { getRandomInteger } from './utils/util.js';
 import { sendData, getData } from './services/api.js';
 import { activateForm, deactivateForm, validateGuests, setAddressValue } from './services/form.js';
 import { initFilter } from './services/filters.js';
+import { pinsNumber } from './variables.js';
 
 deactivateForm(true);
 validateGuests();
@@ -29,40 +30,60 @@ const mainPinMarker = L.marker(
   },
 );
 
-  const onSendSuccess = () => {
-    const success = renderSuccess();
-    const evtListener = document.addEventListener('keydown', () => {
-      success.remove();
-      document.removeEventListener('keydown', evtListener);
-    }, true);
-    success.addEventListener('click', () => success.remove());
-    document.querySelector('body').append(success);
-  };
+const onSendSuccess = () => {
+  const success = renderSuccess();
+  const evtListener = document.addEventListener('keydown', () => {
+    success.remove();
+    document.removeEventListener('keydown', evtListener);
+  }, true);
+  success.addEventListener('click', () => success.remove());
+  document.querySelector('body').append(success);
+};
 
-  const onSendFail = () => {
-    const fail = renderFail();
-    const evtListener = document.addEventListener('keydown', () => {
-      fail.remove();
-      document.removeEventListener('keydown', evtListener);
-    }, true);
-    fail.addEventListener('click', () => fail.remove());
-    document.querySelector('body').append(fail);
-  };
+const onSendFail = () => {
+  const fail = renderFail();
+  const evtListener = document.addEventListener('keydown', () => {
+    fail.remove();
+    document.removeEventListener('keydown', evtListener);
+  }, true);
+  fail.addEventListener('click', () => fail.remove());
+  document.querySelector('body').append(fail);
+};
 
-const handleFormSubmit = function(event) {
+const handleFormSubmit = function (event) {
   event.preventDefault();
   sendData(onSendSuccess, onSendFail, offerForm);
 };
 
 offerForm.addEventListener('submit', handleFormSubmit);
 
-initFilter();
+const handleFilterReady = function (filter) {
+  getData((items) => onGetSuccess(items, filter))
+}
 
-const offersCount = getRandomInteger(0, 40);
-const onGetSuccess = (cardCollection) => {
-  const points = cardCollection.slice(offersCount, offersCount + 10);
-  for (let index = 0; index < points.length; index++) {
-    const card = points[index];
+initFilter(handleFilterReady)
+
+// const offersCount = getRandomInteger(0, 40);
+const onGetSuccess = (cardCollection, filter) => {
+
+  let cards = cardCollection;
+  let filtredArray = [];
+  for (let i = 0; i < cards.length; i++) {
+    let testCard = cards[i];
+    // console.log(testCard.offer.type);
+    // console.log(filter);
+    if (filter.type === testCard.offer.type) {
+      // console.log(filter.type);
+      filtredArray.push(testCard)
+    }
+  }
+
+  filtredArray = filtredArray.slice(0, pinsNumber);
+  console.log(filtredArray)
+  // cards = cards.slice(0, pinsNumber);
+
+  for (let index = 0; index < cards.length; index++) {
+    const card = filtredArray[index];
     const icon = L.icon({
       iconUrl: './img/pin.svg',
       iconSize: [52, 52],
@@ -83,11 +104,22 @@ const onGetSuccess = (cardCollection) => {
         renderCard({
           offer: card.offer,
           author: card.author,
-        }));
+        }),
+        {
+          keepInView: true,
+        },
+
+      );
+    // console.log(filtredArray);
+    // const cleanFilter = function () {
+    //   let filtredArray = [];
+    //   marker.remove();
+    // }
+    // filter.addEventListener('change', cleanFilter);
+
 
   }
 };
-
 getData(onGetSuccess);
 
 setAddressValue(defaultCoordinate);
