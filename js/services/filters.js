5feaab843992a08d9744271423dff8691dff1f6a
Marina Churkina <mapav4@yandex.ru>
2021-07-22 11:27:33 +0500

    Добавлены параметры в фильтры

--- js/services/filters.js
+++ js/services/filters.js
@@ -1,13 +1,17 @@
-const initFilter = () => {
-  let filter = {};
-
+const initFilter = (handleFilterReady) => {
   const handleFilterChange = () => {
+    let filter = {};
     filter.type = document.querySelector('#housing-type').value;
     filter.price = document.querySelector('#housing-price').value;
     filter.rooms = document.querySelector('#housing-rooms').value;
     filter.guests = document.querySelector('#housing-guests').value;
     filter.isWifi = document.querySelector('#filter-wifi').checked;
-    console.log(filter)
+    filter.isDishwasher = document.querySelector('#filter-dishwasher').checked;
+    filter.isParking = document.querySelector('#filter-parking').checked;
+    filter.isWasher = document.querySelector('#filter-washer').checked;
+    filter.isElevator = document.querySelector('#filter-elevator').checked;
+    filter.isConditioner = document.querySelector('#filter-conditioner').checked;
+    handleFilterReady(filter)
   };
 
   let elementsArray = document.querySelectorAll(".map__filter");
@@ -22,7 +26,7 @@ const initFilter = () => {
     elem.addEventListener("click", handleFilterChange);
   });
 
-  return filter;
+
 }
 
 export { initFilter };
