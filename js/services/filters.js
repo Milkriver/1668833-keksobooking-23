fc9d9000b2bf8e43b5e9fb98062530c7cde5e407
Marina Churkina <mapav4@yandex.ru>
2021-07-24 14:46:10 +0500

    Переписана фильтрация features

--- js/services/filters.js
+++ js/services/filters.js
@@ -10,12 +10,7 @@ const rooms = document.querySelector('#housing-rooms');
 const guests = document.querySelector('#housing-guests');
 const type = document.querySelector('#housing-type');
 const price = document.querySelector('#housing-price');
-const dishwasher = document.querySelector('#filter-dishwasher');
-const parking = document.querySelector('#filter-parking');
-const washer = document.querySelector('#filter-washer');
-const elevator = document.querySelector('#filter-elevator');
-const conditioner = document.querySelector('#filter-conditioner');
-const wifi = document.querySelector('#filter-wifi');
+const offerFeatures = Array.from(document.querySelectorAll('.map__checkbox'));
 
 const filterOffers = (offers) => offers.filter(({ offer }) => {
   if (type.value !== 'any' && offer.type !== type.value) {
@@ -27,22 +22,15 @@ const filterOffers = (offers) => offers.filter(({ offer }) => {
       return false;
     }
   }
-  if (wifi.checked && (!offer.features || !offer.features.includes('wifi'))) {
-    return false;
-  }
-  if (dishwasher.checked && (!offer.features || !offer.features.includes('dishwasher'))) {
-    return false;
-  }
-  if (parking.checked && (!offer.features || !offer.features.includes('parking'))) {
-    return false;
-  }
-  if (washer.checked && (!offer.features || !offer.features.includes('washer'))) {
-    return false;
-  }
-  if (elevator.checked && (!offer.features || !offer.features.includes('elevator'))) {
-    return false;
-  }
-  if (conditioner.checked && (!offer.features || !offer.features.includes('conditioner'))) {
+
+  const isEveryFeatureMatches = offerFeatures.every(offerFeature => {
+    if (offerFeature.checked && (!offer.features || !offer.features.includes(offerFeature.value))) {
+      return false;
+    }
+    return true;
+  });
+
+  if(!isEveryFeatureMatches) {
     return false;
   }
 
